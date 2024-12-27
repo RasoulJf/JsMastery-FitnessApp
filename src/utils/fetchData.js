7220840dc8a1980a7be26e69de46eef470e1218f
Amirreza Mirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-12-27 11:13:08 +0330

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,22 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': '614b38f4c6msh514409e296be42dp1ff66cjsne7bb3208b3c0',
-    },
-  };
-  
-  export const youtubeOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
-    },
-  };
-  
-  export const fetchData = async (url, options) => {
-    const res = await fetch(url, options);
-    const data = await res.json();
-  
-    return data;
-  };
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "614b38f4c6msh514409e296be42dp1ff66cjsne7bb3208b3c0",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "614b38f4c6msh514409e296be42dp1ff66cjsne7bb3208b3c0",
+    "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
